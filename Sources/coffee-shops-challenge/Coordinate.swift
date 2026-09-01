51a841254895473f748f51f858372579e72c7cd1
Diana Ciodolan <didi.maria2012@gmail.com>
2026-09-01 09:37:19 +0300

    feat: add distance calculation

--- Sources/coffee-shops-challenge/Coordinate.swift
+++ Sources/coffee-shops-challenge/Coordinate.swift
@@ -10,4 +10,11 @@ import Foundation
 struct Coordinate {
     let x: Double
     let y: Double
+    
+    //distanta euclidiana
+    func distace(to other: Coordinate) -> Double{
+        let deltaX = other.x - x
+        let deltaY = other.y - y
+        return sqrt(deltaX * deltaX + deltaY * deltaY)
+    }
 }
